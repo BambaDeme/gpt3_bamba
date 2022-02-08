3fa2870d29cbfd156f6348d369f178dbc84f8206
bambademe <cheikhbambademe@gmail.com>
2022-02-08 05:20:49 -0800

    file and folder structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import React from "react";
 
+import {
+  Header,
+  Footer,
+  Features,
+  Blog,
+  Possibility,
+  WhatGPT3,
+} from "./containers";
+
+import { CTA, Brand, Navbar } from "./components";
+
+import "./App.css";
 const App = () => {
   return (
     <div>
-      <h1>GPT3</h1>
+      <div className="App">
+        <div className="gradient__bg">
+          <Navbar />
+          <Header />
+        </div>
+        <Brand />
+        <WhatGPT3 />
+        <Features />
+        <Possibility />
+        <CTA />
+        <Blog />
+        <Footer />
+      </div>
     </div>
   );
 };
